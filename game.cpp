475d31641e9d15e4cd869fe1b680708179107423
Annie Kuxa <kuxa.business@gmail.com>
2025-07-08 11:42:34 -0300

    Change for loop to decrementing guesses.

--- game.cpp
+++ game.cpp
@@ -16,7 +16,7 @@ bool playGame(int guesses) {
     //std::cout << "Correct # is: " << correct << std::endl;
     std::cout << "You get " << guesses << " guesses" << std::endl;
 
-    for (int guessCount = 0; guessCount < guesses; guessCount++) {
+    for (int guessesLeft = guesses; guessesLeft > 0; guessesLeft--) {
         std::cout << "Guess a number..." << std::endl;
         int guess;
         std::cin >> guess;
@@ -27,7 +27,7 @@ bool playGame(int guesses) {
         } else {
             std::cout << "Too high mate! " << guess << std::endl;
         }
-        std::cout << "You've guessed " << guessCount+1 << " times." << std::endl;
+        std::cout << "You've have " << guessesLeft-1 << " guesses left." << std::endl;
     }
     return false;
 }
