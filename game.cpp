e14044a8c15d574af6d0590f17406a4ad4f7dfd7
Annie Kuxa <kuxa.business@gmail.com>
2025-07-08 11:32:54 -0300

    Change while loop to for loop for main functionality.

--- game.cpp
+++ game.cpp
@@ -14,22 +14,20 @@ bool playGame(int guesses) {
     }
 
     //std::cout << "Correct # is: " << correct << std::endl;
-    int guessCount = 0;
     std::cout << "You get " << guesses << " guesses" << std::endl;
-    while(guessCount < guesses) {
-        std::cout << "Guess a number..." << std::endl;
 
+    for (int guessCount = 0; guessCount < guesses; guessCount++) {
+        std::cout << "Guess a number..." << std::endl;
         int guess;
         std::cin >> guess;
         if (guess == correct) {
             return true;
         } else if (guess < correct) {
-            std::cout << "Too low mate!" << guess << std::endl;
+            std::cout << "Too low mate! " << guess << std::endl;
         } else {
-            std::cout << "Too high mate!" << guess << std::endl;
+            std::cout << "Too high mate! " << guess << std::endl;
         }
-        guessCount++;
-        std::cout << "You've guessed " << guessCount << " times." << std::endl;
+        std::cout << "You've guessed " << guessCount+1 << " times." << std::endl;
     }
     return false;
 }
