3d87c8f90bc49cd1aab988e83c98ff91d0562df5
Annie Kuxa <kuxa.business@gmail.com>
2025-07-08 11:22:45 -0300

    Refactor the code, add header file

--- main.cpp
+++ main.cpp
@@ -1,37 +1,7 @@
 #include <iostream>
 #include <string>
 #include <cstdlib>
-
-bool playGame(int guesses) {
-    std::cout << "Playing game..." << std::endl;
-    int correct;
-    srand(time(NULL));
-    if (guesses == 1) {
-        correct = rand() % 200;
-    } else {
-        correct = rand() % 20;
-    }
-
-    std::cout << "Correct # is: " << correct << std::endl;
-    int guessCount = 0;
-        std::cout << "You get " << guesses << " guesses" << std::endl;
-    while(guessCount < guesses) {
-        std::cout << "Guess a number..." << std::endl;
-
-        int guess;
-        std::cin >> guess;
-        if (guess == correct) {
-            return true;
-        } else if (guess < correct) {
-            std::cout << "Too low mate!" << guess << std::endl;
-        } else {
-            std::cout << "Too high mate!" << guess << std::endl;
-        }
-        guessCount++;
-        std::cout << "You've guessed " << guessCount << " times." << std::endl;
-    }
-    return false;
-}
+#include "game.h"
 
 int main() {
 
